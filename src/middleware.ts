36b1900e6add38f9231fe9898a07f53d828862dc
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-09-15 17:51:31 +0530

    first

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export default clerkMiddleware(async (auth, request) => {
   }
 })
 
+// below is the config file!!!
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
