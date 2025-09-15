f510fdddd5330ebfef26685640c53590db7b663f
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2025-09-15 15:38:51 +0530

    second chnge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// this for chnge 2
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
